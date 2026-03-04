65bd1e4ffe497188679dfcc5604d57eaea834b9b
codeBuddha <jamwujustyle@gmail.com>
2026-03-04 21:13:28 +0500

    scaffold makefile, test around

--- golambda/main.go
+++ golambda/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
 
 func main() {
-	fmt.Println("placeholder")
+	lambda.Start(handler)
+}
+
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	log.Println("hello world")
+
+	response:= events.APIGatewayProxyResponse{
+		StatusCode: 200,
+	}
+	return response, nil
 }
